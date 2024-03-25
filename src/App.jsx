e25b2e355dc2f11d5dbc785adae5b468bdc913e2
beehsouza-th <brenda.souza@estudante.ifms.edu.br>
2024-03-25 08:39:02 -0400

    verdade

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Main from "./Components/Main";
 import "./globals.css";
 export default function App(){
-  return(
-    <main>
+ return(
+    <div>
     <Header/>
     <Footer/>
-    </main>
+    <Main/> 
+    </div>
   );
-
-  
 }
 
 
