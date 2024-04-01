3cc298b9da489b873ace22db8319177189964c30
beehsouza-th <brenda.souza@estudante.ifms.edu.br>
2024-04-01 11:38:27 -0400

    Novo Estado

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,57 +1,74 @@
  import { useState } from "react";
-     export default function Main(){
+
+export default function Main() {
      const [nome, setNome] = useState(""); 
-     const [telefone, setTelefone] = useState(); 
-     const [listaContatos, setContatos] = useState ([]);
+     const [telefone, setTelefone] = useState(0);
+     const [email, setEmail] = useState(""); 
+     const [listaContatos, setListaContatos] = useState([]);
 
 
-     const registrar = () => {
-     alert("deu certo");
+     const registrar = (event) => {
+     event.preventDefault();  //remover  
+     alert("aamewm");
+
+     setListaContatos([...listaContatos,
+     {
+          nomeSalvo: nome,
+          telefoneSalvo: telefone,
+          emailSalvo: email
      }
+     ]);
+   
+};
+     console.table(listaContatos); //adicionar
+
 return(
-<main>
-<form> 
+ <main>
+    <form>
+ <div>
+     <label htmlFor = "nome"> Nome :</label>
+          <input type = "text"  nome = "nome-contato"  id = "nome" value = {nome} onChange={(event) => setNome(event.target.value)}/>
+          {nome}
 
-     <label htmlFor ="nome"> Nome:</label>
-     <input type ="text"name="contato-nome"id="nome" value= {nome} onChange={  (event)  => setNome(event.target.value)}/>
-     {nome}
+     <label htmlFor = "telefone"> Telefone :</label>
+          <input type = "text"  nome = "telefone-contato"  id = "telefone" value = {telefone} onChange={(event) => setTelefone(event.target.value)}/>
+          {telefone}
 
-     <label htmlFor ="telefone"> Telefone:</label>
-<input type ="tel"name="contato-nome"id="telefone"value= {telefone} onChange={  (event)  => setTelefone(event.target.value)}/>
-{telefone}
+     <label htmlFor = "email"> Email :</label>
+     <input type = "text"  nome = "email-contato"  id = "email" value = {email} onChange={(event) => setEmail(event.target.value)}/>
+     {email}
 
-     <button id="bot"> Salvar </button>
-</form>
+     <button class = "botao" > Salvar </button>
+     </div>
+     </form>
+    
 </main>
-
-);}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
